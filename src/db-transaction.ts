1f40a9092ccb5a480279a3e79164afbc82ad105d
Ezeki Zibzibadze <ez@jok.io>
2017-12-22 09:50:29 +0400

    exported ProcessTransaction interface

--- src/db-transaction.ts
+++ src/db-transaction.ts
@@ -1,8 +1,10 @@
 import * as Datastore from '@google-cloud/datastore'
 import { DatastoreTransaction } from '@google-cloud/datastore/transaction';
 
+export type ProcessTransaction = (tran: DatastoreTransaction) => Promise<any>
+
 export const configureTransaction = (datastore: Datastore) => {
-	return async (process: (tran: DatastoreTransaction) => Promise<any>) => {
+	return async (process: ProcessTransaction) => {
 		const tran = datastore.transaction();
 
 		try {
