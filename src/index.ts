1d5db79bdda96607c639c226928ae21cc467d003
Ezeki Zibzibadze <ez@jok.io>
2017-12-24 23:35:57 +0400

    exported db-set-base

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import * as GoogleDatastore from '@google-cloud/datastore'
 
 export * from './types'
 export * from './db-transaction'
+export * from './db-set-base'
 export * from './db-set'
 export * from './db-context'
 export * from './db-aggregate'
